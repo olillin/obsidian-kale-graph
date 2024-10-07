29cccf6fcd111701c84754f72d1673200be4ed78
Oli <oli@olillin.com>
2024-10-07 19:24:04 +0200

    Create sample markdown postprocessor

--- main.ts
+++ main.ts
@@ -1,134 +1,89 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian'
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface KaleGraphSettings {
+    codeBlockKeyword: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KaleGraphSettings = {
+    codeBlockKeyword: 'kale'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class KaleGraph extends Plugin {
+    settings: KaleGraphSettings
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // This adds a settings tab so the user can configure various aspects
+        // of the plugin.
+        this.addSettingTab(new KaleGraphSettingTab(this.app, this))
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.registerMarkdownCodeBlockProcessor(
+            this.settings.codeBlockKeyword,
+            markdownPostProcessor
+        )
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    onunload() {
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        )
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings)
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class KaleGraphSettingTab extends PluginSettingTab {
+    plugin: KaleGraph
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: KaleGraph) {
+        super(app, plugin)
+        this.plugin = plugin
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    display(): void {
+        const {containerEl} = this
+
+        containerEl.empty()
+
+        containerEl.createEl('h2', 'Code blocks')
+        new Setting(containerEl)
+            .setName('Kale Graph Keyword')
+            .setDesc('Will render a kale graph view if provided as the '
+                    +'language of a code block.')
+            .addText(text => text
+                .setPlaceholder('Enter your secret')
+                .setValue(this.plugin.settings.codeBlockKeyword)
+                .onChange(async (value) => {
+                    this.plugin.settings.codeBlockKeyword = value
+                    await this.plugin.saveSettings()
+                }))
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function markdownPostProcessor(source, el, ctx) {
+    const rows = source.split("\n").filter((row) => row.length > 0);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    const table = el.createEl("table");
+    const body = table.createEl("tbody");
 
-	display(): void {
-		const {containerEl} = this;
+    for (let i = 0; i < rows.length; i++) {
+      const cols = rows[i].split(",");
 
-		containerEl.empty();
+      const row = body.createEl("tr");
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      for (let j = 0; j < cols.length; j++) {
+        row.createEl("td", { text: cols[j] });
+      }
+    }
 }
