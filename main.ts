df3489f9cb488f86be6ce2c89bcc5b317f467fd4
Oli <oli@olillin.com>
2024-10-07 20:13:03 +0200

    Parse code block

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian'
+import {
+    App,
+    Plugin,
+    PluginSettingTab,
+    Setting,
+    MarkdownPostProcessorContext,
+} from "obsidian"
 
 // Remember to rename these classes and interfaces!
 
@@ -7,7 +13,7 @@ interface KaleGraphSettings {
 }
 
 const DEFAULT_SETTINGS: KaleGraphSettings = {
-    codeBlockKeyword: 'kale'
+    codeBlockKeyword: "kale",
 }
 
 export default class KaleGraph extends Plugin {
@@ -26,9 +32,7 @@ export default class KaleGraph extends Plugin {
         )
     }
 
-    onunload() {
-
-    }
+    onunload() {}
 
     async loadSettings() {
         this.settings = Object.assign(
@@ -52,38 +56,101 @@ class KaleGraphSettingTab extends PluginSettingTab {
     }
 
     display(): void {
-        const {containerEl} = this
+        const { containerEl } = this
 
         containerEl.empty()
 
-        containerEl.createEl('h2', 'Code blocks')
+        containerEl.createEl("h2", "Code blocks")
         new Setting(containerEl)
-            .setName('Kale Graph Keyword')
-            .setDesc('Will render a kale graph view if provided as the '
-                    +'language of a code block.')
-            .addText(text => text
-                .setPlaceholder('Enter your secret')
-                .setValue(this.plugin.settings.codeBlockKeyword)
-                .onChange(async (value) => {
-                    this.plugin.settings.codeBlockKeyword = value
-                    await this.plugin.saveSettings()
-                }))
+            .setName("Kale Graph Keyword")
+            .setDesc(
+                "Will render a kale graph view if provided as the " +
+                    "language of a code block."
+            )
+            .addText((text) =>
+                text
+                    .setPlaceholder("Enter your secret")
+                    .setValue(this.plugin.settings.codeBlockKeyword)
+                    .onChange(async (value) => {
+                        this.plugin.settings.codeBlockKeyword = value
+                        await this.plugin.saveSettings()
+                    })
+            )
     }
 }
 
-function markdownPostProcessor(source, el, ctx) {
-    const rows = source.split("\n").filter((row) => row.length > 0);
+interface Flags {
+    directed: boolean
+    simple: boolean
+}
 
-    const table = el.createEl("table");
-    const body = table.createEl("tbody");
+const DEFAULT_FLAGS: Flags = {
+    directed: false,
+    simple: false,
+}
 
-    for (let i = 0; i < rows.length; i++) {
-      const cols = rows[i].split(",");
+const FLAG_PREFIX = "-"
 
-      const row = body.createEl("tr");
+type Vertex = string
+type Edge = [Vertex, Vertex]
 
-      for (let j = 0; j < cols.length; j++) {
-        row.createEl("td", { text: cols[j] });
-      }
+const EDGE_VALIDATE_PATTERN =
+    /^[({[]?([({[]\s*(\w+?\s*[,]\s*\w+?)\s*[)}\]](\s*[,]\s*)?)+[)}\]]?$/g
+const EDGE_SEARCH_PATTERN = /[({[]\s*(\w+?)\s*[,]\s*(\w+?)\s*[)}\]]/g
+const VERTEX_SEARCH_PATTERN = /\w+/g
+
+function markdownPostProcessor(
+    source: string,
+    el: HTMLElement,
+    ctx: MarkdownPostProcessorContext
+) {
+    const lines = source
+        .split("\n")
+        .map((line) => line.trim())
+        .filter((line) => line.length > 0)
+    if (lines.length == 0) return
+
+    const flags: Flags = Object.assign({}, DEFAULT_FLAGS)
+    const vertices: Array<Vertex> = []
+    const edges: Array<Edge> = []
+
+    for (let line of lines) {
+        if (line.startsWith(FLAG_PREFIX)) {
+            Object.assign(flags, parseFlags(line))
+        } else if (EDGE_VALIDATE_PATTERN.test(line)) {
+            for (let match of line.matchAll(EDGE_SEARCH_PATTERN)) {
+                edges.push([match[1], match[2]])
+            }
+        } else {
+            for (let match of line.matchAll(VERTEX_SEARCH_PATTERN)) {
+                vertices.push(match[0])
+            }
+        }
     }
+
+    const flagsList = el.createEl("ul", { text: "Flags" })
+    const verticesList = el.createEl("ul", { text: "Vertices" })
+    const edgesList = el.createEl("ul", { text: "Edges" })
+
+    Object.entries(flags).forEach((flag) => {
+        flagsList.createEl("li", { text: `${flag[0]}: ${flag[1]}` })
+    })
+
+    vertices.forEach((vertex) => {
+        verticesList.createEl("li", { text: vertex })
+    })
+
+    console.log(edges)
+    edges.forEach((edge) => {
+        edgesList.createEl("li", { text: edge.join(",") })
+    })
+}
+
+function parseFlags(line: string): Partial<Flags> {
+    const flags: Partial<Flags> = {}
+
+    if (line.contains("d")) flags.directed = true
+    if (line.contains("s")) flags.simple = true
+
+    return flags
 }
